46eb3bad6e4905fa141d5ff1847be9a982612824
ksm0207 <ksm03071@naver.com>
2021-05-23 16:41:54 +0900

    스프링 게시판 프로젝트 생성

---
+++ Board/src/main/java/com/BoardTest/demoBoardTest/DemoBoardTestApplication.java
@@ -0,0 +1,13 @@
+package com.BoardTest.demoBoardTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBoardTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBoardTestApplication.class, args);
+	}
+
+}
---
+++ Board/src/test/java/com/BoardTest/demoBoardTest/DemoBoardTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BoardTest.demoBoardTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBoardTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
